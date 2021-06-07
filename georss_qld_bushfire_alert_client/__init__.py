5ea3edcab5a258118de8e530bf222ac7a3149c31
Malte Franken <exxamalte@users.noreply.github.com>
2021-06-07 22:05:05 +1000

    Using Black (#8)
    * black configuration
    
    * black formatted

--- georss_qld_bushfire_alert_client/__init__.py
+++ georss_qld_bushfire_alert_client/__init__.py
@@ -7,53 +7,64 @@ from georss_client import GeoRssFeed, FeedEntry, ATTR_ATTRIBUTION
 from georss_client.consts import CUSTOM_ATTRIBUTE
 from georss_client.feed_manager import FeedManagerBase
 
-REGEXP_ATTR_STATUS = 'Current Status: (?P<{}>[^<]+)[\n\r]'\
-    .format(CUSTOM_ATTRIBUTE)
+REGEXP_ATTR_STATUS = "Current Status: (?P<{}>[^<]+)[\n\r]".format(CUSTOM_ATTRIBUTE)
 
 URL = "https://www.qfes.qld.gov.au/data/alerts/bushfireAlert.xml"
 
-VALID_CATEGORIES = ['Emergency Warning', 'Watch and Act', 'Advice',
-                    'Notification', 'Information']
+VALID_CATEGORIES = [
+    "Emergency Warning",
+    "Watch and Act",
+    "Advice",
+    "Notification",
+    "Information",
+]
 
 
 class QldBushfireAlertFeedManager(FeedManagerBase):
     """Feed Manager for Qld Bushfire Alert feed."""
 
-    def __init__(self, generate_callback, update_callback, remove_callback,
-                 coordinates, filter_radius=None,
-                 filter_categories=None):
+    def __init__(
+        self,
+        generate_callback,
+        update_callback,
+        remove_callback,
+        coordinates,
+        filter_radius=None,
+        filter_categories=None,
+    ):
         """Initialize the Qld Bushfire Alert Feed Manager."""
         feed = QldBushfireAlertFeed(
             coordinates,
             filter_radius=filter_radius,
-            filter_categories=filter_categories)
-        super().__init__(feed, generate_callback, update_callback,
-                         remove_callback)
+            filter_categories=filter_categories,
+        )
+        super().__init__(feed, generate_callback, update_callback, remove_callback)
 
 
 class QldBushfireAlertFeed(GeoRssFeed):
     """Qld Bushfire Alert feed."""
 
-    def __init__(self, home_coordinates, filter_radius=None,
-                 filter_categories=None):
+    def __init__(self, home_coordinates, filter_radius=None, filter_categories=None):
         """Initialise this service."""
         super().__init__(home_coordinates, URL, filter_radius=filter_radius)
         self._filter_categories = filter_categories
 
     def _new_entry(self, home_coordinates, rss_entry, global_data):
         """Generate a new entry."""
-        attribution = None if not global_data and ATTR_ATTRIBUTION not in \
-            global_data else global_data[ATTR_ATTRIBUTION]
-        return QldBushfireAlertFeedEntry(home_coordinates, rss_entry,
-                                         attribution)
+        attribution = (
+            None
+            if not global_data and ATTR_ATTRIBUTION not in global_data
+            else global_data[ATTR_ATTRIBUTION]
+        )
+        return QldBushfireAlertFeedEntry(home_coordinates, rss_entry, attribution)
 
     def _filter_entries(self, entries):
         """Filter the provided entries."""
         entries = super()._filter_entries(entries)
         if self._filter_categories:
-            return list(filter(lambda entry:
-                               entry.category in self._filter_categories,
-                               entries))
+            return list(
+                filter(lambda entry: entry.category in self._filter_categories, entries)
+            )
         return entries
 
 
