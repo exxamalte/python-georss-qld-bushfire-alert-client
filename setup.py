2a08935f58908dae4a18ce1b1136ed05b7f1c76c
Malte Franken <exxamalte@users.noreply.github.com>
2024-02-14 08:22:24 +1100

    Add Python 3.11 support (#25)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIRES = [
     "georss_client>=0.15",
 ]
 
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
